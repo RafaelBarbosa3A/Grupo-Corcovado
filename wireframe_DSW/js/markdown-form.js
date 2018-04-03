2002e9e270f5f48342dc717fac8f8d7ba2ed3132
Diego Matsuki <diego.matsuki@gmail.com>
2018-04-02 21:44:56 -0300

    Wireframes DSW

---
+++ wireframe_DSW/js/markdown-form.js
@@ -0,0 +1,24 @@
+/*
+  Ao clicar no botão (ou link) o preview é renderizado dentro da div (substituindo seu conteúdo) de preview.
+  Criar um botão ou link com o atributo class="button-md", input com class="textarea-md" e um div com id="preview-md".
+*/
+
+const md = window.markdownit("default", {
+  html: true,
+  breaks: true,
+  linkify: true,
+  typographer: true
+})
+
+md.renderer.rules.table_open = (tokens, idx) => {
+  return '<table class="table table-bordered table-striped">'
+}
+
+md.renderer.rules.blockquote_open = (tokens, idx) => {
+  return '<blockquote class="blockquote mb-2">'
+}
+
+document.querySelector(".button-md").addEventListener("click", ev => {
+  ev.preventDefault()
+  document.querySelector("#preview-md").innerHTML = md.render(document.querySelector(".textarea-md").value);
+})
---
+++ wireframe_DSW/js/markdown-show.js
@@ -0,0 +1,23 @@
+/*
+  Renderiza o texto em outra tag, escondendo a primeira.
+  Criar uma tag com o atributo class="to_render-md" e um div com id="rendered-md".
+*/
+
+const md = window.markdownit("default", {
+  html: true,
+  breaks: true,
+  linkify: true,
+  typographer: true
+})
+
+md.renderer.rules.table_open = (tokens, idx) => {
+  return '<table class="table table-bordered table-striped">'
+}
+
+md.renderer.rules.blockquote_open = (tokens, idx) => {
+  return '<blockquote class="blockquote mb-2">'
+}
+
+let to_render = document.querySelector(".to_render-md")
+to_render.classList.add("d-none")
+document.querySelector("#rendered-md").innerHTML = md.render(to_render.textContent)
